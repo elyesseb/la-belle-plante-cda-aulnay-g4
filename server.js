224e4d473c0fae764b5c933c5fa55ecd1843102f
ufhopla <elyesse.benhadj@gmail.com>
2022-03-03 14:31:57 +0100

    test jb

--- server.js
+++ server.js
@@ -3,10 +3,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/la-belle-plante-cda-aulnay-g4"));
+app.use(express.static(__dirname + "/dist/la-belle-plante"));
 app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname + "/dist/la-belle-plante-cda-aulnay-g4/index.html")
-  );
+  res.sendFile(path.join(__dirname + "/dist/la-belle-plante/index.html"));
 });
 app.listen(process.env.PORT || 8080);
